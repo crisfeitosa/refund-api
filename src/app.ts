b8cee6917065bee4ab59bfb39d31343b45635e7b
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-06 22:43:49 -0300

    Tratamento de exceções

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 
+import { errorHandling } from "@/middlewares/error-handling";
+import { AppError } from "./utils/AppError";
+
 const app = express();
 
 app.use(cors());
@@ -9,5 +12,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(errorHandling);
 
 export { app };
