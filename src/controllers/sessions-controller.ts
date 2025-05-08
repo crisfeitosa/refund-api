c553e6176f0c69d84d53f4edf4ab61eb0d23775d
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-08 19:00:47 -0300

    Validando dados da sessions

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from 'express';
+import { z } from 'zod';
 
 class SessionController {
   async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string(),
+    });
+
+    const { email, password } = bodySchema.parse(request.body);
+
     response.json({ message: 'Session created' });
   }
 }
