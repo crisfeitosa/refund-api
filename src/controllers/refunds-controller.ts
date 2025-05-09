fbb4a173fe2389591a18f35d23670ad82031ae5c
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-09 20:41:23 -0300

    Criando rota de listar

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -37,6 +37,12 @@ class RefundsController {
     })
 
     response.status(201).json(refund);
+  };
+
+  async index(request: Request, response: Response) {
+    response.json({
+      message: "List of refunds"
+    });
   }
 }
 
