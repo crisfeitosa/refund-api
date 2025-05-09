34dd7a4aa0500d63b30c16fafbae51dc1eee2644
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-09 19:57:30 -0300

    Validação de dados da solicitação

--- src/middlewares/verify-user-authorization.ts
+++ src/middlewares/verify-user-authorization.ts
@@ -1,8 +1,8 @@
 import { AppError } from '@/utils/AppError';
-import { Request, Response, NextFunction, request } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 function verifyUserAuthorization(role: string[]) {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return (request: Request, response: Response, next: NextFunction) => {
     if(!request.user || !role.includes(request.user.role)) {
       throw new AppError('Unauthorized', 401);
     } 
