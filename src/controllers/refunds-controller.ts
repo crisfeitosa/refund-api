f55969df3531af6de4d664abd1dc5736c3da14c7
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-10 14:39:22 -0300

    Exibindo uma solicitação especifica

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -88,6 +88,25 @@ class RefundsController {
       }
     });
   }
+
+  async show(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      id: z.string().uuid()
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const refund = await prisma.refunds.findFirst({
+      where: { id },
+      include: { user: true }
+    });
+
+    if (!refund) {
+      throw new AppError("Reembolso não encontrado", 404);
+    }
+
+    response.json(refund);
+  }
 }
 
 export { RefundsController };
